d712a749b6e1bcdb66b0a1f17416e2cd91b834a8
rebel228 <dinovik@rambler.ru>
2023-04-16 22:05:56 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.Stack = []
+  }
+
+  push(e) {
+    this.Stack.push(e);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.Stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.Stack.slice(-1).pop();
   }
 }
 
