2218455164e5b2c39d5880597cb406275466db50
rebel228 <dinovik@rambler.ru>
2023-04-17 01:23:27 +0300

    feat: queue

--- src/queue.js
+++ src/queue.js
@@ -13,23 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+class Queue {
+  constructor(value) {
+    this.head;
+    this.Node = class {
+      constructor(value) {
+        this.value = value;
+        this.next = null;
+      }
+    }
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    if (!this.head) this.head = new this.Node(value);
+    else {
+      let curr = this.head;
+      while (curr.next) {
+        curr = curr.next;
+      }
+      curr.next = new this.Node(value);
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.head.value;
+    this.head = this.head.next;
+    return res;
   }
+
 }
+
+
 
 module.exports = {
   Queue
